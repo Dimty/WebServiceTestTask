7b6b6a78498a17ef033af02e88b6ba8307819ffc
Dimty <nikitin-dmitriyy@mail.ru>
2022-10-09 14:43:13 +0600

    Skeleton 1.0

--- WebServiceTestTask/Program.cs
+++ WebServiceTestTask/Program.cs
@@ -2,11 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/api/mails", () => "asdas");
-app.MapPost("/api/mails", () =>
-{
-    
-});
 app.Map("/",async (context) =>
 {
     await context.Response.SendFileAsync("wwwroot/index.html");
